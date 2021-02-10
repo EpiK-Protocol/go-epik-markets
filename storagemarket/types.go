a76e97b02a069622a5f4752987502d8b53982b62
dirkmc <dirkmdev@gmail.com>
2021-02-10 09:52:12 +0100

    feat: add RawBlockSize to DataRef (#487)

--- storagemarket/types.go
+++ storagemarket/types.go
@@ -181,8 +181,9 @@ type DataRef struct {
 	TransferType string
 	Root         cid.Cid
 
-	PieceCid  *cid.Cid              // Optional for non-manual transfer, will be recomputed from the data if not given
-	PieceSize abi.UnpaddedPieceSize // Optional for non-manual transfer, will be recomputed from the data if not given
+	PieceCid     *cid.Cid              // Optional for non-manual transfer, will be recomputed from the data if not given
+	PieceSize    abi.UnpaddedPieceSize // Optional for non-manual transfer, will be recomputed from the data if not given
+	RawBlockSize uint64                // Optional: used as the denominator when calculating transfer %
 
 	Expert string
 }
